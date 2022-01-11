c1f5a61c34ab94986ea0692f18e02f1fc1a501df
DavidSKoppel <davi3442@edu.sde.dk>
2022-01-11 10:44:00 +0100

    initial commit

---
+++ ShoppingList/ShoppingList/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ShoppingList.ViewModels;
+using ShoppingList.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ShoppingList
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
